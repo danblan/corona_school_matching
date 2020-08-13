fa2c70c580a18d195ae5e1b0c75b238ab2f5d1e6
danblan <daniel.blankenburg@gmx.net>
2020-08-13 16:53:26 +0200

    Commented out local test code in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include "graph_creator.h"
 #include "matching.h"
 #include "nlohmann/json.hpp"
+#include "stats.h"
 using json = nlohmann::json;
 
 void write_example_into_json() {
-
     json example = {
             {"students", {
                     {{"Bundesland", "NRW"}, {"Fächer",{{ {"Fach","Deutsch"},{"Klassenstufe", {5,6,7,8}}}}}},
@@ -22,15 +22,15 @@ void write_example_into_json() {
 
 int main() {
     //READ DATA AND CONSTRUCT GRAPH
-    write_example_into_json();
-    CS::GraphCreator gc;
-    std::ifstream fstream("../examples/first_input.json");
-    gc.init_from_json(fstream);
-    std::vector<CS::Edge> matching_edges;
-    CS::compute_max_cost_matching(gc, matching_edges);
-    for (auto const & edge : matching_edges) {
-        std::cout<<edge.pupil_id <<" "<<edge.college_student_id<<std::endl;
-    }
+    ///Local test:
+    //CS::GraphCreator gc;
+    //std::ifstream pupfstream("C:/Users/danie/Downloads/data/data/datasets/05_08_2020_13_39_16/tutees.json");
+    //std::ifstream stufstream("C:/Users/danie/Downloads/data/data/datasets/05_08_2020_13_39_16/tutors.json");
+    //gc.init_from_json(pupfstream, stufstream);
+    //std::vector<CS::Edge> matching_edges;
+    //std::cout<< CS::compute_max_cost_matching(gc, matching_edges)<<std::endl;
+    //CS::test_matching_valid(matching_edges, gc.nodes());
+    //CS::dump_matching_edges_into_json(matching_edges, gc);
     //ADAPT PRICES MAYBE AND RESTART
     return 0;
 }
